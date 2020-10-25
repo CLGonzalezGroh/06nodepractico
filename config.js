002142d96101c17f261b690c8705aaaeedac7303
Cesar Gonzalez Groh <clgonzalezgroh@hotmail.com>
2020-10-25 12:50:37 -0300

    mysql creada y conectada

--- config.js
+++ config.js
@@ -5,4 +5,10 @@ module.exports = {
   jwt: {
     secret: process.env.JWT_SECRET || "notASecret!",
   },
+  mysql: {
+    host: process.env.MYSQL_HOST || "remotemysql.com",
+    user: process.env.MYSQL_USER || "3xn4QJYhZJ",
+    password: process.env.MYSQL_PASS || "RMeZpgbiHY",
+    database: process.env.MYSQL_DB || "3xn4QJYhZJ",
+  },
 };
